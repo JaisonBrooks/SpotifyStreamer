2524f0cb3bd4a0f963a42121990b62fd1dcc47a6
Jaison Brooks <jbrooks@enphaseenergy.com>
2015-06-16 10:26:14 -0600

    updated both activity with toolbar and navigation between is working

--- app/src/main/java/jaisonbrooks/spotifystreamer/ArtistSearchResults.java
+++ app/src/main/java/jaisonbrooks/spotifystreamer/ArtistSearchResults.java
@@ -1,9 +1,12 @@
 package jaisonbrooks.spotifystreamer;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ArtistSearchResults extends AppCompatActivity {
 
@@ -11,6 +14,14 @@ public class ArtistSearchResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_search_results);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
